2333b8c59a90acd12461dd1de8ec28659ef02b29
Seif Elfayoumy <seifelfayoumy@gmail.com>
2022-12-31 23:33:20 +0200

    completed most of the system

--- index.js
+++ index.js
@@ -1,24 +1,214 @@
 const express = require('express');
+const session = require('express-session');
 const { MongoClient } = require('mongodb');
+const path = require('path');
 
 const app = new express();
+app.use(session({
+  secret: 'my-secret-for-this-networks-project',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(express.urlencoded({ extended: false }));
+
 app.set('view engine', 'ejs');
-const url = 'mongodb+srv://seif:mydatabasepassword@cluster0.s7gamqu.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(url);
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, '/public')))
+
+const dbUrl = 'mongodb+srv://seif:mydatabasepassword@cluster0.s7gamqu.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(dbUrl);
 main();
 
-// app.post('/signup', async (req,res) => {
-//   console.log(req.body);
-//   const users = client.db().collection('User');
-//   users.insertOne({
-//     name
-//   })
-//   res.status(200).send('hello world');
-// });
+app.post('/registration', async (req, res) => {
+  try {
+    const users = client.db().collection('users');
 
-// app.get('/', async(req,res) => {
-//   res.render('./views/home.ejs');
-// })
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username) {
+      throw new Error('username is required');
+    }
+    if (!password) {
+      throw new Error('password is required');
+    }
+
+    const userExists = await users.countDocuments({ username });
+
+    if (userExists != 0) {
+      throw new Error('username already exists')
+    }
+
+    await users.insertOne({
+      username,
+      password,
+      wanttogo: []
+    })
+
+    res.render('login', {
+      message: "registration was successful."
+    });
+  } catch (e) {
+    res.render('registration', {
+      message: e.message
+    });
+  }
+
+});
+
+
+
+
+app.post('/login', async (req, res) => {
+  try {
+    const users = client.db().collection('users');
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username) {
+      throw new Error('username is required');
+    }
+    if (!password) {
+      throw new Error('password is required');
+    }
+
+    const userExists = await users.countDocuments({ username, password });
+
+    if (userExists != 1) {
+      throw new Error('wrong credentials')
+    }
+
+    req.session.username = username;
+    res.redirect('/home');
+  } catch (e) {
+    res.render('login', {
+      message: e.message
+    });
+  }
+})
+
+app.post('/wanttogo/:destname', async (req, res) => {
+  const destName = req.params.destname;
+  try {
+    const users = client.db().collection('users');
+    const user = await users.findOne({ username: req.session.username });
+
+    if (!user) {
+      throw new Error('please login first');
+    }
+
+    if (user.wanttogo.includes(destName)) {
+      throw new Error('destination is already in your list')
+    }
+
+    await users.updateOne({ username: req.session.username }, {
+      $push: { wanttogo: destName }
+    })
+
+    res.render(destName, {
+      message: destName + ' was added to your want to go list successfully'
+    })
+
+  } catch (e) {
+    res.render(destName, {
+      message: e.message
+    })
+  }
+})
+
+app.post('/search', async(req,res) => {
+  const search = req.body.Search;
+  const desinations = ['annapurna', 'bali', 'inca', 'paris', 'rome', 'santorini'];
+
+  const newDestinations = desinations.filter(d => d.includes(search));
+
+  res.render('searchresults',{
+    desinations: newDestinations
+  })
+})
+
+app.get('/registration', async (req, res) => {
+  res.render('registration', {
+    message: ""
+  });
+})
+
+app.get('/login', (req,res) =>{
+  res.render('login', {
+    message: ""
+  })
+})
+
+app.get('/home', (req,res) =>{
+  if(req.session.username){
+    res.render('home', {
+      username: req.session.username
+    })
+  }else{
+    res.redirect('/registration')
+  }
+
+})
+
+app.get('/', (req, res) => {
+  if(req.session.username){
+    res.redirect('/home');
+  }
+  res.redirect('/registration');
+})
+
+app.get('/hiking', (req, res) => {
+  res.render('hiking');
+})
+app.get('/cities', (req, res) => {
+  res.render('cities');
+})
+app.get('/islands', (req, res) => {
+  res.render('islands');
+})
+app.get('/inca', (req, res) => {
+  res.render('inca', {
+    message: ""
+  });
+})
+app.get('/annapurna', (req, res) => {
+  res.render('annapurna', {
+    message: ""
+  });
+})
+app.get('/paris', (req, res) => {
+  res.render('paris', {
+    message: ""
+  });
+})
+app.get('/rome', (req, res) => {
+  res.render('rome', {
+    message: ""
+  });
+})
+app.get('/bali', (req, res) => {
+  res.render('bali', {
+    message: ""
+  });
+})
+app.get('/santorini', (req, res) => {
+  res.render('santorini', {
+    message: ""
+  });
+})
+app.get('/wanttogo', async (req, res) => {
+  try{
+    const users = client.db().collection('users');
+    const user = await users.findOne({username: req.session.username});
+    res.render('wanttogo', {
+      desinations: user.wanttogo
+    });
+  }catch(e){
+    res.redirect('/');
+  }
+
+})
 
 app.listen(3000, () => {
   console.log('server is running on port 3000');
@@ -26,5 +216,5 @@ app.listen(3000, () => {
 
 async function main() {
   await client.connect();
-  console.log('Connected successfully to server');
+  console.log('Connected successfully to database');
 }
